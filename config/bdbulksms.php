04e8317ff0ffed41eaf1175303093f6c30e69708
Greenweb Bangladesh <51988436+bdgreenweb@users.noreply.github.com>
2025-08-18 14:26:25 +0600

    change url for response processing

--- config/bdbulksms.php
+++ config/bdbulksms.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    'api_url' => 'https://api.bdbulksms.net/api.php',
+    'api_url' => 'https://api.bdbulksms.net/api.php?json',
     'token'   => env('BD_BULKSMS_TOKEN', ''),
     'log_retention_days' => env('BD_BULKSMS_LOG_RETENTION', 30),
 ];
